b752815c057ce9315a59621c9a9dbdba57d16cd2
seubseub <seub0730@gmail.com>
2018-12-13 03:42:28 +0900

    에디터 뷰어 관련 테스트

--- RichEditorViewSample/RichEditorViewSample/ViewerController.swift
+++ RichEditorViewSample/RichEditorViewSample/ViewerController.swift
@@ -7,24 +7,90 @@
 //
 
 import UIKit
+import RichEditorView
 
 class ViewerController: UIViewController {
 
+    @IBOutlet weak var richViewer: RichViewer!
+    
+//    lazy var toolbar: RichEditorToolbar = {
+//        let toolbar = RichEditorToolbar(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 44))
+//        toolbar.options = RichEditorDefaultOption.all
+//        return toolbar
+//    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+//        richViewer.delegate = self
+//        richViewer.inputAccessoryView = toolbar
+//        richViewer.placeholder = "Type some text..."
+        richViewer.html = "dsabfkdbsaklfblksdaldflskafbkldsabfkladsbklfbdsaklfbklsal<img src=\"http://cdnetphoto.appphotocard.com/boards/18/1230927/60588/e8c7f4d31139111985b98caa2e217bb3.jpg\" alt=\"photo\"><img src=\"http://cdnetphoto.appphotocard.com/boards/18/1230927/60588/12ff23e280088e2695d21272cc6d8e6c.jpg\" alt=\"photo\">"
+        
+//        toolbar.delegate = self
+//        toolbar.editor = editorView
+//
+//        // We will create a custom action that clears all the input text when it is pressed
+//        let item = RichEditorOptionItem(image: nil, title: "Clear") { toolbar in
+//            toolbar.editor?.html = ""
+//        }
+//
+//        var options = toolbar.options
+//        options.append(item)
+//        toolbar.options = options
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+//extension ViewerController: RichViewerDelegate {
+//
+////    func richEditor(_ editor: RichEditorView, contentDidChange content: String) {
+////        if content.isEmpty {
+////            htmlTextView.text = "HTML Preview"
+////        } else {
+////            htmlTextView.text = content
+////        }
+////    }
+//    func richEditor(_ editor: RichViewer, heightDidChange height: Int) {
+//        print("height \(height)")
+//    }
+//
+//}
+
+//extension ViewerController: RichEditorToolbarDelegate {
+//
+//    fileprivate func randomColor() -> UIColor {
+//        let colors: [UIColor] = [
+//            .red,
+//            .orange,
+//            .yellow,
+//            .green,
+//            .blue,
+//            .purple
+//        ]
+//
+//        let color = colors[Int(arc4random_uniform(UInt32(colors.count)))]
+//        return color
+//    }
+//
+//    func richEditorToolbarChangeTextColor(_ toolbar: RichEditorToolbar) {
+//        let color = randomColor()
+//        toolbar.editor?.setTextColor(color)
+//    }
+//
+//    func richEditorToolbarChangeBackgroundColor(_ toolbar: RichEditorToolbar) {
+//        let color = randomColor()
+//        toolbar.editor?.setTextBackgroundColor(color)
+//    }
+//
+//    func richEditorToolbarInsertImage(_ toolbar: RichEditorToolbar) {
+//        toolbar.editor?.insertImage("https://gravatar.com/avatar/696cf5da599733261059de06c4d1fe22", alt: "Gravatar")
+//    }
+//
+//    func richEditorToolbarInsertLink(_ toolbar: RichEditorToolbar) {
+//        // Can only add links to selected text, so make sure there is a range selection first
+//        if toolbar.editor?.hasRangeSelection == true {
+//            toolbar.editor?.insertLink("http://github.com/cjwirth/RichEditorView", title: "Github Link")
+//        }
+//    }
+//}
